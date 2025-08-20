f7c7dabc3e8bd57220e657b2b0d08e17af7f92a3
kunal vashistha <vashisthakunal7@gmail.com>
2025-08-20 11:37:49 +0530

    Update chat frontend script

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ async function sendMessage() {
 
     try {
         // Send request to backend
-        const response = await fetch("https://resumebackend-1-gzp6.onrender.com/api/v1/resume", {
+        const response = await fetch("http://localhost:3000/api/v1/resume", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ message })
